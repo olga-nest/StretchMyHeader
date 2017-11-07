5a6c40bc569b192d4dffadcc31f3b1b8b48477aa
olga-nest <olga.w.nesterova@gmail.com>
2017-11-07 11:44:30 -0800

    Create NewsItem struct

---
+++ StretchMyHeader/NewsItem.swift
@@ -0,0 +1,23 @@
+//
+//  NewsItem.swift
+//  StretchMyHeader
+//
+//  Created by Olga on 11/7/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+import Foundation
+
+struct NewsItem {
+    
+    struct Category {
+        static let world = "World"
+        static let americas = "Americas"
+        static let europe = "Europe"
+        static let middleEast = "Middle East"
+        static let africa  = "Africa"
+        static let asia = "Asia Pacific"
+        
+    }
+    
+}
